be133514a77ac8041e3e54e1c6f515e83815e317
Imran Khan <ik521992@outlook.com>
2014-07-25 07:22:32 -0700

    Upload Files

---
+++ mad_twitter/js/mad_twitter.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+       $("#mad_twitter_1").hover(function(){
+          $("#mad_twitter_1 .fa-twitter").toggleClass("magictime foolishIn");
+        });
+
+        $("#mad_twitter_2").hover(function(){
+          $("#mad_twitter_2 .fa-twitter").toggleClass("magictime twisterInDown");
+        });
+
+        $("#mad_twitter_3").hover(function(){
+          $("#mad_twitter_3 .fa-twitter").toggleClass("magictime magic");
+        });
+       $("#mad_twitter_4").hover(function(){
+          $("#mad_twitter_4 .fa-twitter").toggleClass("magictime bombLeftOut");
+        });
+
+        $("#mad_twitter_5").hover(function(){
+          $("#mad_twitter_5 .fa-twitter").toggleClass("magictime tinUpIn");
+        });
+
+        $("#mad_twitter_6").hover(function(){
+          $("#mad_twitter_6 .fa-twitter").toggleClass("magictime tinRightOut");
+        });
+
+
+
+
+
+});
